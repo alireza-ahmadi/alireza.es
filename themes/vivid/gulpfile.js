38af646cc87a68613b30579a45bb5dc1f79d5291
Alireza Ahmadi <a.ahmadi94@gmail.com>
2020-06-18 12:22:12 +0430

    Upgrade Gulp to work with latest NodeJS version

--- themes/vivid/gulpfile.js
+++ themes/vivid/gulpfile.js
@@ -1,33 +1,35 @@
-const gulp = require('gulp')
-const util = require('gulp-util')
-const cleanCSS = require('gulp-clean-css')
-const less = require('gulp-less')
-const autoprefixer = require('gulp-autoprefixer')
+const gulp = require("gulp");
+const util = require("gulp-util");
+const cleanCSS = require("gulp-clean-css");
+const less = require("gulp-less");
+const autoprefixer = require("gulp-autoprefixer");
 
 // Utilities
 const errorHandler = (error) => {
-  util.log(util.colors.red, 'ERROR:', error.message)
-  this.emit('end')
-}
+  util.log(util.colors.red, "ERROR:", error.message);
+  this.emit("end");
+};
 
 // Compile LESS files
-gulp.task('less', () => {
+gulp.task("less", (done) => {
   const prefixOptions = {
-    browsers: 'last 20 versions'
+    overrideBrowserslist: "last 20 versions",
   };
 
-  gulp.src('static/styles/style.less')
+  gulp
+    .src("static/styles/style.less")
     .pipe(less())
-    .on('error', errorHandler)
+    .on("error", errorHandler)
     .pipe(autoprefixer(prefixOptions))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('static/styles'));
+    .pipe(gulp.dest("static/styles"));
+  done();
 });
 
 // Watch changes
-gulp.task('watch', () => {
-  gulp.watch('static/styles/**/*.less', ['less']);
+gulp.task("watch", () => {
+  gulp.watch("static/styles/**/*.less", gulp.series(["less"]));
 });
 
-gulp.task('build', ['less']);
-gulp.task('default', ['build', 'watch']);
+gulp.task("build", gulp.series(["less"]));
+gulp.task("default", gulp.series(["build", "watch"]));
